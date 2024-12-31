9f828be162ddf6b5b7f5d448d1f0124aaf532839
xiaohuohumax <xiaohuohumax@users.noreply.github.com>
2024-12-31 05:24:36 +0000

    build: :package: build user script

--- dist/test-script-publish.user.js
+++ dist/test-script-publish.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         test-script-publish
 // @namespace    vite-plugin-monkey
-// @version      1.0.17
+// @version      1.0.18
 // @author       xiaohuohumax
 // @description  test script publish
 // @license      MIT
