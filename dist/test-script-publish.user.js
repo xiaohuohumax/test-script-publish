d4db773cf0ccd015b8f8c02479fd6349f5f3cedc
xiaohuohumax <xiaohuohumax@users.noreply.github.com>
2024-12-31 05:10:16 +0000

    release: :rocket: release version

--- dist/test-script-publish.user.js
+++ dist/test-script-publish.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         test-script-publish
 // @namespace    vite-plugin-monkey
-// @version      1.0.15
+// @version      1.0.16
 // @author       xiaohuohumax
 // @description  test script publish
 // @license      MIT
